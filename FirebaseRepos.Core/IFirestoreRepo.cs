f0ddad826f4b1b2cfa059a6eebae3e1ccd8368a3
youssef shaaban <ysfme2@hotmail.com>
2021-10-15 03:33:14 +0200

    adding .net core support

---
+++ FirebaseRepos.Core/IFirestoreRepo.cs
@@ -0,0 +1,16 @@
+﻿using Firebase.Database;
+using Firebase.Database.Streaming;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FirebaseRepos.Core.Reposatories
+{
+    public interface IFirestoreRepo<T> : IRepository<T>
+    {
+        Task<List<T>> GetAsync(List<(string fieldName, object value)> filterFields);
+
+        void SetListener(Action<T> action);
+        void SetListener<B>(Action<B> action, string path) where B : new();
+    }
+}
