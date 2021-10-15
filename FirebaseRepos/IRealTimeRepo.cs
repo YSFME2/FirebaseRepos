0ff6d2bf46c0c5c0474b0877161de5d5c9cab5a1
youssef shaaban <ysfme2@hotmail.com>
2021-10-15 02:43:55 +0200

    Add project files.

---
+++ FirebaseRepos/IRealTimeRepo.cs
@@ -0,0 +1,15 @@
+﻿using Firebase.Database;
+using Firebase.Database.Streaming;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FirebaseRepos.Reposatories
+{
+    public interface IRealTimeRepo<T> : IRepository<T>
+    {
+        Task<List<T>> GetAsync(Func<FirebaseObject<T>, bool> predec);
+        void SetListener(Action<FirebaseEvent<T>> action);
+        void SetListener<B>(Action<FirebaseEvent<B>> action, string path);
+    }
+}
